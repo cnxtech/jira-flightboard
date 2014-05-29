43f67fe17938fc6b958df7512a3eddcebb3ed011
toni.lopez <toni.lopez@shazam.com>
2014-05-29 11:40:42 +0100

    ITL-358: Fixing loader.js rootpoint.

--- web/js/loader.js
+++ web/js/loader.js
@@ -1,11 +1,11 @@
-function load(dataEndpoint, templateName, interval) {
+function load(type, interval) {
     var templateType = $(window).width() < 500 ? "mobile" : "wall";
-    var templatePath = "./web/templates/" + templateType + "/" + templateName + ".mst";
+    var templatePath = "./web/templates/" + templateType + "/" + type + ".mst";
 
     $.get(templatePath, function (templateContent) {
         setInterval(
             function () {
-                $("#content").load(dataEndpoint, function (response) {
+                $("#content").load("./api/" + type + ".php", function (response) {
                     var data = JSON.parse(response);
                     var content = Mustache.render(templateContent, data);
                     $(this).html(content);
