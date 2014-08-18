0d1efd6b30e125ad6e845116cb0772cc7c0020c9
toni.lopez <toni.lopez@shazam.com>
2014-08-18 15:16:23 +0100

    ITL-524: Fixing problem when getting pages get param.

--- epics.php
+++ epics.php
@@ -2,5 +2,5 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
-$page = isset($_GET['page']) ? (int)  $_GET : 0;
+$page = isset($_GET['page']) ? (int)  $_GET['page'] : 0;
 echo $app['twig']->render('epics.twig', array('page' => $page));
